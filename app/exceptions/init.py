8f7538c5e539294186340cf407d44a63662bd103
mohamadhoseinsamadi <msmdy7194@gmail.com>
2026-01-02 21:44:23 +0330

    chore: add custom exceptions and seed scripts

---
+++ app/exceptions/init.py
@@ -0,0 +1,17 @@
+from app.exceptions.custom_exceptions import (
+    NotFoundError,
+    ValidationError,
+    ConflictError,
+    UnauthorizedError,
+    ForbiddenError,
+    ServerError
+)
+
+__all__ = [
+    "NotFoundError",
+    "ValidationError",
+    "ConflictError",
+    "UnauthorizedError",
+    "ForbiddenError",
+    "ServerError"
+]
